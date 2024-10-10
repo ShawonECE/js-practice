9e193ce903ea4fc4103554b894d9ca27492d0b99
ShawonECE <send2shawon@gmail.com>
2024-10-10 13:56:12 +0600

    alternate solution added

--- leetcode/countStudents.js
+++ leetcode/countStudents.js
@@ -10,5 +10,23 @@ const countStudents = (students, sandwiches) => {
     return sandwiches.length;
 };
 
-const students = [1,1,0,0], sandwiches = [0,1,0,1];
+// const countStudents = (students, sandwiches) => {
+//     let studentsTop = 0;
+//     let sandwichesTop = 0;
+//     while (students.includes(sandwiches[sandwichesTop])) {
+//         if (students[studentsTop] === 'e') {
+//             studentsTop === students.length - 1 ? studentsTop = 0 : studentsTop++;
+//         }
+//         if (sandwiches[sandwichesTop] === students[studentsTop]) {
+//             sandwichesTop++;
+//             students[studentsTop] = 'e';
+//             studentsTop === students.length - 1 ? studentsTop = 0 : studentsTop++; 
+//         } else {
+//             studentsTop === students.length - 1 ? studentsTop = 0 : studentsTop++;
+//         }
+//     }
+//     return sandwiches.length - sandwichesTop;
+// };
+
+const students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1];
 console.log(countStudents(students, sandwiches));
