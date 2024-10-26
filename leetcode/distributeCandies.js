428033a8226606aa9261d0b9789aaf47d7a75f93
ShawonECE <send2shawon@gmail.com>
2024-10-26 17:40:21 +0600

    1 combinatorics solution added

---
+++ leetcode/distributeCandies.js
@@ -0,0 +1,35 @@
+const distributeBetweenTwo = (n, limit) => {
+    let ways = 0;
+    if (n === 0) {
+        return 0;
+    }
+    for (let i = Math.min(n, limit); i >= 0 ; i--) {
+        if (i > n - i) {
+            ways += 2;
+        } else if (i === n - i) {
+            ways += 1;
+            return ways;
+        } else {
+            return ways;
+        }
+    }
+};
+
+// console.log(distributeBetweenTwo(3, 2));
+
+const distributeCandies = (n, limit) => {
+    if (n > 3 * limit) {
+        return 0;
+    }
+    let ways = 0;
+    for (let i = Math.min(limit, n); i >= 0; i--) {
+        for (let j = Math.min(limit, n - i); j >= 0; j--) {
+            if (n - i - j >= 0 && n - i - j <= limit) {
+                ways++;
+            }
+        }
+    }
+    return ways;
+};
+
+console.log(distributeCandies(5, 2));
--- leetcode/tempCodeRunnerFile.js
+++ leetcode/tempCodeRunnerFile.js
@@ -1,5 +1,13 @@
-let point1 = points[i];
-                let point2 = points[j];
-                let point3 = points[k];
-                let area = 0.5 * Math.abs(point1[0] * (point2[1] - point3[1]) + point2[0] * (point3[1] - point1[1]) + point3[0] * (point1[1] - point2[1]))
-                maxArea = Math.max(maxArea, area);
+const distributeBetweenTwo = (n, limit) => {
+    let ways = 0;
+    for (let i = n - limit; i <= limit ; i++) {
+        if (i < n - i) {
+            ways += 2;
+        } else if (i === n - i) {
+            ways += 1;
+            return ways;
+        } else {
+            return ways;
+        }
+    }
+};
